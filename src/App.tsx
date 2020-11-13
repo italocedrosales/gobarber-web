6e260cc5d2f4e289361d4bcc33a3971927f0d79f
Italo Cedro <italo.cedro.sales@gmail.com>
2020-11-13 13:05:49 -0300

    Eslint, Prettier, EditorConfig and GlobalStyles

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+
+      <h1>Hello Gostack</h1>
+    </>
+  );
+};
+
+export default App;
