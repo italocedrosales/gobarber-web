dfef50d9a5e1895e43534f8ba5cde1adc67226b8
Italo Cedro <italo.cedro.sales@gmail.com>
2020-11-16 21:30:22 -0300

    Login using Context API

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => {
   return (
     <>
-      <AuthContext.Provider value={{ name: 'Italo' }}>
+      <AuthProvider>
         <SignIn />
-      </AuthContext.Provider>
+      </AuthProvider>
 
       <GlobalStyle />
     </>
